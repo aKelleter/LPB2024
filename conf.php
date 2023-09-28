b11857d091a6a492fc257ede7b9136fbb67656e5
Alain Kelleter <alain.kelleter@kelleter.be>
2023-09-28 15:22:32 +0200

    Update v1.0.4
    Add and configure a favicon

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 
-    const APP_UPDATE = '28/09/2023 13:05';
-    const APP_VERSION = 'v1.0.3';
+    const APP_UPDATE = '28/09/2023 15:20';
+    const APP_VERSION = 'v1.0.4';
     const APP_YEAR = '2024';
         
     function displayFooter($year, $date, $version)
