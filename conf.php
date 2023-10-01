129aee8af724bb79feb65d4c94878a39b92e71e1
Alain Kelleter <alain.kelleter@kelleter.be>
2023-10-01 20:45:42 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 
-    const APP_UPDATE = '01/10/2023 20:25';
-    const APP_VERSION = 'v1.3.0';
+    const APP_UPDATE = '01/10/2023 20:45';
+    const APP_VERSION = 'v1.3.2';
     const APP_YEAR = '2024';
     
     // Inclusion des fonctions
