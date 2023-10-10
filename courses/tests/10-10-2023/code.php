69c640fda5c3d9afbc0cd90a4ceb967af662f74f
Alain Kelleter <alain.kelleter@kelleter.be>
2023-10-10 21:18:06 +0200

    Minor Update

--- courses/tests/10-10-2023/code.php
+++ courses/tests/10-10-2023/code.php
@@ -1,1 +1,23 @@
 <?php
+    $a = "Bonjour";
+    echo "$a<br>";
+
+    $a .= " le monde";
+
+    echo "$a<br>";
+
+    /*
+    $a = "Bonjour" . " le monde";    
+    echo "$a<br>";
+    */
+
+    $string = '';
+
+    $string .= '<div>';
+    $string .= 'Hello';
+    $string .= '</div>';
+
+    echo $string;
+
+
+
