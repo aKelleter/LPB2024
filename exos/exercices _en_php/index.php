413fe149cce60692f0a5f8d30980bb7976db657b
Alain Kelleter <alain.kelleter@kelleter.be>
2023-11-28 21:07:27 +0100

    Update index.php

--- exos/exercices _en_php/index.php
+++ exos/exercices _en_php/index.php
@@ -55,7 +55,7 @@
         <footer>        
             <div class="row mt-5">
                 <div class="col-md-12 text-center">
-                    <p class="text-footer"><?php echo getHtmlFooter(APP_YEAR, APP_UPDATE, APP_VERSION, '../'); ?></p>
+                    <p class="text-footer"><?php echo getHtmlFooter(APP_YEAR, APP_UPDATE, APP_VERSION, '../../'); ?></p>
                 </div>
             </div>        
         </footer> 
