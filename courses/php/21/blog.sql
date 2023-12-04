6c623abdadbd18ff189567a3e39353d8ef4567ec
Alain Kelleter <alain.kelleter@kelleter.be>
2023-12-04 15:53:21 +0100

    Update export blog.sql

--- courses/php/21/blog.sql
+++ courses/php/21/blog.sql
@@ -1,105 +1,58 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost
--- Généré le : dim. 26 nov. 2023 à 16:34
--- Version du serveur : 10.4.28-MariaDB
--- Version de PHP : 8.2.4
+/*
+ Navicat MariaDB Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : VPS ORION
+ Source Server Type    : MariaDB
+ Source Server Version : 100521 (10.5.21-MariaDB-0+deb11u1)
+ Source Host           : 94.176.233.50:3306
+ Source Schema         : blog
 
+ Target Server Type    : MariaDB
+ Target Server Version : 100521 (10.5.21-MariaDB-0+deb11u1)
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 04/12/2023 15:35:21
+*/
 
---
--- Base de données : `blog`
---
-CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `blog`;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `articles`
---
-
+-- ----------------------------
+-- Table structure for articles
+-- ----------------------------
 DROP TABLE IF EXISTS `articles`;
-CREATE TABLE `articles` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `title` varchar(250) NOT NULL,
-  `content` longtext NOT NULL,
-  `picture` varchar(250) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `articles`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `picture` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `active` tinyint(3) UNSIGNED NULL DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Déchargement des données de la table `articles`
---
+-- ----------------------------
+-- Records of articles
+-- ----------------------------
+INSERT INTO `articles` VALUES (1, 'L&#039;hiver est à nos portes', 'L’été nous apporte ses journées ensoleillées et ses soirées dorées ; l’automne, le\r\nmouvement du vent et la coloration glorieuse du feuillage. Mais l’hiver ?\r\nSon doux manteau de neige, uniforme et épuré, recouvre les aspérités des paysages,\r\nleur permettant de prendre une pause pour mieux renaître au printemps.', NULL, 1);
+INSERT INTO `articles` VALUES (2, 'Le soleil', 'Le Soleil est l’étoile autour de laquelle tournoient tous les objets qui constituent notre système solaire, à savoir les planètes, les comètes et autres astéroïdes.', NULL, 1);
+INSERT INTO `articles` VALUES (3, 'L\'intelligence artificielle', 'Souvent classée dans le groupe des mathématiques et des sciences cognitives, elle fait appel à la neurobiologie computationnelle (particulièrement aux réseaux neuronaux) et à la logique mathématique (partie des mathématiques et de la philosophie). ', NULL, 1);
+INSERT INTO `articles` VALUES (15, 'Encore un', '...', NULL, 0);
 
-INSERT INTO `articles` (`id`, `title`, `content`, `picture`) VALUES
-(1, 'L\'hiver est à nos portes', 'L’été nous apporte ses journées ensoleillées et ses soirées dorées; l’automne, le\r\nmouvement du vent et la coloration glorieuse du feuillage. Mais l’hiver?\r\nSon doux manteau de neige, uniforme et épuré, recouvre les aspérités des paysages,\r\nleur permettant de prendre une pause pour mieux renaître au printemps.', NULL),
-(2, 'Le soleil', 'Le Soleil est l’étoile autour de laquelle tournoient tous les objets qui constituent notre système solaire, à savoir les planètes, les comètes et autres astéroïdes.', NULL),
-(3, 'L\'intelligence artificielle', 'Souvent classée dans le groupe des mathématiques et des sciences cognitives, elle fait appel à la neurobiologie computationnelle (particulièrement aux réseaux neuronaux) et à la logique mathématique (partie des mathématiques et de la philosophie). ', NULL);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `passwd` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `users`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `passwd` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Déchargement des données de la table `users`
---
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'john@mail.com', '1234');
+INSERT INTO `users` VALUES (2, 'alain@kelleter.be', 'AZERTY');
 
-INSERT INTO `users` (`id`, `email`, `passwd`) VALUES
-(1, 'john@mail.com', '1234'),
-(2, 'alain@kelleter.be', 'AZERTY');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `articles`
---
-ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `articles`
---
-ALTER TABLE `articles`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
