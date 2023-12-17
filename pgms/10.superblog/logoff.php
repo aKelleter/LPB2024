b5ae92d5aa89f7a83c594efc09d0e722bddbe65a
Alain Kelleter <alain.kelleter@kelleter.be>
2023-12-17 16:22:31 +0100

    Update 0.1.1

--- pgms/10.superblog/logoff.php
+++ pgms/10.superblog/logoff.php
@@ -1,8 +1,12 @@
 <?php
-
+    require_once('settings.php');
+    
     // Script de déconnexion
     unset($_SESSION);
+    setcookie(session_name(), '', 100);
     session_destroy();
+
+    session_name(APP_NAME);
     session_start();
     $_SESSION['IDENTIFY'] = false;
     header('Location: index.php');
--- pgms/10.superblog/settings.php
+++ pgms/10.superblog/settings.php
@@ -1,19 +1,19 @@
 <?php
+    // Constantes de l'application
+    const APP_NAME = "SuperBlog";
+    const APP_VERSION = 'v0.1.1';
+    const APP_UPDATED = '17-12-2023 16:20';
+    const APP_AUTHOR = 'Joyeuses Fêtes';
 
     // Ouverture de la session et initialisation de la variable $_SESSION['IDENTIFY']
+    session_name(APP_NAME);
     session_start();
     if (!isset($_SESSION['IDENTIFY'])) {
         $_SESSION['IDENTIFY'] = false;
     }
     
-    // Constantes de l'application
-    const APP_NAME = "Superblog";
-    const APP_VERSION = 'v0.0.1';
-    const APP_UPDATED = '13-12-2023 20:45';
-    const APP_AUTHOR = 'Vous :)';
-
     // Constante d'activation/désactivation du mode DEBUG
-    const DEBUG = true;
+    const DEBUG = false;
 
     // Charge les credentials de connexion à la DB
     require_once('conf/conf-db.php');
