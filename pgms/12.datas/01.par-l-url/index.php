936cf50d402c6c28fe17094b41065f5780d7a00f
Alain Kelleter <alain.kelleter@kelleter.be>
2023-12-23 17:26:25 +0100

    Update index.php

--- pgms/12.datas/01.par-l-url/index.php
+++ pgms/12.datas/01.par-l-url/index.php
@@ -3,14 +3,29 @@
     require_once '../fct.php';
 
     // Affichage brute du tableau superglobal $_GET
-    disp_ar($_GET, '$_GET');
+    disp_ar($_GET, 'Tableau     $_GET');
+
+    $nom = null;
+    $age = null;
+
+    /**
+     * isset($var) : variable $var existe et est différente de null
+     * empty($var) : variable $var est vide
+     * !empty($var) : variable $var n'est PAS vide
+     * header("Location : page.php") : redirige la page en cours vers la page page.php
+     * header("refresh:2;url=page.php") : redirige la page en cours après 2sec vers la page page.php
+     * is_object($var) :  verifié que $var est un objet
+     * is_array($var) : vérifie que $ var est un array / tableau
+     * 
+     */
 
     // On test si on a reçu un nom par l'url en testant la clé "nom" dans $_GET
     if(isset($_GET['nom']) && !empty($_GET['nom']))
     {
         echo 'Nom : ' . $_GET['nom'].'<br>';
         $nom = $_GET['nom'];
-    }
+    }else
+        echo 'nom est vide<br>';
 
     // On test si on a reçu un nom par l'url en testant la clé "age" dans $_GET
     if(isset($_GET['age']) && !empty($_GET['age']))
