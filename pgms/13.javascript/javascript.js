9ae824fb105857fd5159eb62fb1f194fb4b6103f
Alain Kelleter <alain.kelleter@kelleter.be>
2024-01-23 16:09:40 +0100

    Update 2.5.3

--- pgms/13.javascript/javascript.js
+++ pgms/13.javascript/javascript.js
@@ -15,7 +15,7 @@ if(path === "root") {
 /**
  * Gestion du footer
  */
-let version = "0.5.2";
-let updated = "22/01/2024 18:05";
+let version = "0.5.3";
+let updated = "23/01/2024 16:30";
 let footer = document.querySelector('footer');
 footer.innerHTML = `Le cours de Javascript v${version} - Updated: ${updated} - LPB 2024 - <a href="#top">Top</a>`;
